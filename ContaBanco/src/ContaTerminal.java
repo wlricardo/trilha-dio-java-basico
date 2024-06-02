b9073fc11980e185cfc9c8c955771057f9e4d2b2
wlricardo <wlricardo@gmail.com>
2024-06-02 10:34:01 -0300

    Alterando o código segundo especificações.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência : ");
+        String numero = sc.nextLine();
+        System.out.print("Por favor, informe a agência : ");
+        String agencia = sc.nextLine();
+        System.out.print("Por favor, informe o nome do cliente : ");
+        String nome = sc.nextLine();
+        System.out.print("Por favor, informe o saldo : R$ ");
+        float saldo = sc.nextFloat();
+
+        System.out.println("\nOlá " + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia
+                + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");
+
+        sc.close();
     }
 }
